b424df6635d9137002416bb505b359348bf9da40
seviherrmi <seviherrmi@gmail.com>
2022-01-20 15:36:29 +0100

    added view of blogs by search and some for authentication

--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -1,30 +1,41 @@
 import "./sidebar.css"
 import person from "./../../image/person.jpg"
+import { useEffect, useState } from "react"
+import axios from "axios"
+import { Link } from "react-router-dom"
 
 export default function Sidebar() {
+    const [cats, setCats] = useState([])
+
+    useEffect(() => {
+        const getCats = async () => {
+            const res = await axios.get("/categories")
+            setCats(res.data)
+        }
+        getCats()
+    }, [])
     return (
         <div className="sidebar">
             <div className="sidebarItem">
                 <span className="sidebarTitle">ABOUT ME</span>
-                <img className="sidebarImg" src={person} alt="missing img"/>
+                <img className="sidebarImg" src={person} alt="missing img" />
                 <p className="sidebarText">
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
-                It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
-                and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                    when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                    It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                    It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+                    and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
                 </p>
             </div>
             <div className="sidebarItem">
                 <span className="sidebarTitle">CATEGORIES</span>
                 <ul className="sidebarList">
-                    <li className="sidebarListItem">Life</li>
-                    <li className="sidebarListItem">Music</li>
-                    <li className="sidebarListItem">Style</li>
-                    <li className="sidebarListItem">Sport</li>
-                    <li className="sidebarListItem">Tech</li>
-                    <li className="sidebarListItem">Cinema</li>
+                    {cats.map((c) => (
+                        <Link to={`/?cat=${c.name}`} className="link">
+                            <li className="sidebarListItem">{c.name}</li>
+                        </Link>
+                    ))}
                 </ul>
             </div>
             <div className="sidebarItem">
--- client/src/components/singlePost/SinglePost.jsx
+++ client/src/components/singlePost/SinglePost.jsx
@@ -1,5 +1,5 @@
 import "./singlePost.css"
-import { useLocation } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 import { useEffect } from "react"
 import axios from "axios"
 import { useState } from "react"
@@ -10,20 +10,20 @@ export default function SinglePost() {
     const path = location.pathname.split("/")[2];
     const [post, setPost] = useState({})
 
-    useEffect(()=>{
-        const getPost = async ()=>{
+    useEffect(() => {
+        const getPost = async () => {
             const res = await axios.get("/posts/" + path)
             setPost(res.data)
         }
         getPost()
-    },[path]);
+    }, [path]);
     return (
         <div className="singlePost">
             <div className="singlePostWrapper">
                 {post.photo && (
                     <img className="singlePostImg" src={post.photo} alt="missing img" />
                 )}
-                
+
                 <h1 className="singlePostTitle">
                     {post.title}
                     <div className="singlePostEdit">
@@ -32,7 +32,11 @@ export default function SinglePost() {
                     </div>
                 </h1>
                 <div className="singlePostInfo">
-                    <span className="singlePostAuthor">Author: <b>{post.username}</b></span>
+                    <span className="singlePostAuthor">Author:
+                        <Link to={`/?user=${post.username}`} className="link">
+                            <b>{post.username}</b>
+                        </Link>
+                    </span>
                     <span className="singlePostDate">{new Date(post.createdAt).toDateString()}</span>
                 </div>
                 <p className="singlePostDesc">
