3a3cab612f974b70476f27cee877268fa04d18b2
Severin Herrmann <seviherrmi@gmail.com>
2022-01-21 09:14:26 +0100

    fixed image problem and changed backgrounds

--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./sidebar.css";
+import image from "./../../images/profile.jpg"
 
 export default function Sidebar() {
   const [cats, setCats] = useState([]);
@@ -17,8 +18,8 @@ export default function Sidebar() {
     <div className="sidebar">
       <div className="sidebarItem">
         <span className="sidebarTitle">ABOUT ME</span>
-        <img
-          src="https://i.pinimg.com/236x/1e/3f/58/1e3f587572a7a7b20bbf1828595a1786--holiday-party-themes-holiday-gift-guide.jpg"
+        <img className="sidebarImg"
+          src={image}
           alt=""
         />
         <p>
